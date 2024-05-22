2f876c991d9b8e9dc34d7e4104c8c6fab9b45a1b
jep <jeffreybonina05@gmail.com>
2024-05-22 20:01:08 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-"# web-kdj" 
-"# web-kdj" 
+"# web-final" 
--- home.html/script.js
+++ home.html/script.js
@@ -1,29 +1,35 @@
- let sections=document.querySelectorAll('section');
+const section=document.querySelectorAll('section');
 
- window.onscroll= () => {
-   sections.forEach(sec =>{
-        let top=window.scrollY ;
-        let offset=sec.offsetTop -150;
-        let height=sec.offsetHeight;
+window.onscroll= () =>{
+   section.forEach(sec=>{
 
-        if(top >offset && top <offset +height){
-            sec.classList.add('show-animation');
-        }
-        else{
-            sec.classList.remove('show-animation');
-        }   
+  let top=window.scrollY;
+  let height=sec.offsetHeight -150;
+  let offset=sec.offsetTop;
+
+  if(top>= offset && top < offset +height){
+     sec.classList.add('show-animate');
+  }
+  else{
+    sec.classList.remove('show-animate');
+  }
+
    })
- }
+}
 
- let next=document.querySelector('.next');
- let prev=document.querySelector('.prev');
 
- next.addEventListener('click',function(){
-  let items=document.querySelectorAll('.item')
-  document.querySelector('.slide').appendChild(items[0])
- })
- prev.addEventListener('click',function(){
-  let items=document.querySelectorAll('.item')
-  document.querySelector('.slide').prepend(items[items.length-1])
- })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
